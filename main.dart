54bff73204ed55ff5bcc962ab738b6640555ab9e
Anas-Awni <anoosalkhabbas@gmail.com>
2022-08-26 03:42:48 +0300

    Updated By Anas Awni

--- main.dart
+++ main.dart
@@ -1,4 +1,22 @@
 void main() {
-  String myname = "Anas";
-  String lastname = "Khabbas";
+  Map anasInformation = {
+    "first_name": "Anas",
+    "last_name": "Awni",
+    "age": 24,
+    "nationallity": "Jordanian",
+    "FriendsList": [
+      {
+        "first": "belal",
+        "second": "huthaifa",
+        "third": "mohammed",
+        "fourth": "osama",
+        "lastTwo": ["tareq", "yazan"]
+      }
+    ],
+    "education info": {
+      "name": "Al albayt university",
+      "faculty": "IT",
+      "year": 2022,
+    }
+  };
 }
